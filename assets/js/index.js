1de56be904af55749ef8a6f46c8894543d14c09a
kali <feliphe@tuta.io>
2023-05-24 00:12:49 +0100

    Future

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,11 @@
 "use strict";
 const menuMobile = document.querySelector("[data-header-mobile-nav]");
 const menuMobileBtn = document.querySelector("[data-header-mobile-menu-nav]");
+const menuMobileClose = document.querySelector("[data-header-mobile-fa-x-btn]");
 menuMobile.addEventListener("click", () => {
     if (menuMobileBtn.classList.add("open")) {
 
-    }
-})
+}})
+menuMobileClose.addEventListener("click", () => {
+    if(menuMobileBtn.classList.remove("open")) {
+}})
